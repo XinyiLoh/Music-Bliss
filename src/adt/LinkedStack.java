18039098495489c7a515157daf303357368917ea
HuiHuiKoh <kohhh-wm19@student.tarc.edu.my>
2021-08-27 18:43:39 +0800

    Update LinkedStack.java

--- src/adt/LinkedStack.java
+++ src/adt/LinkedStack.java
@@ -8,7 +8,66 @@ package adt;
 /**
  *
  * @author Koh Hui Hui
+ * @param <T>
  */
-public class LinkedStack<T extends Comparable<T>> implements StackInterface<T> {
+public class LinkedStack<T> implements StackInterface<T> {
 
+    private Node topNode;
+    private int numberEntry;
+
+    public LinkedStack() {
+        topNode = null;
+        numberEntry = 0;
+    }
+
+    @Override
+    public void push(T newEntry) {
+        Node newNode = new Node(newEntry);
+        newNode.next = topNode;
+        topNode = newNode;
+    }
+
+    @Override
+    public T pop() {
+        T result = peek();
+        if (topNode != null) {
+            topNode = topNode.next;
+        }
+        return result;
+    }
+
+    @Override
+    public T peek() {
+        T result = null;
+        if (topNode != null) {
+            result = topNode.data;
+        }
+        return result;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return topNode == null;
+    }
+
+    @Override
+    public void clear() {
+        topNode = null;
+    }
+
+    private class Node {
+
+        private T data;
+        private Node next;
+
+        private Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+
+        private Node(T data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
 }
